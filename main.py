a489cb1d24182574baaf15b87712501b81c82e3c
littlepanger <helingyun@justtruth.cn>
2020-07-30 12:05:44 +0800

    add todo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         path = sys.argv[1]
     else:
-        path = './TestCase/picihuafen.json'
+        path = './TestCase/test.json'
     if not os.path.exists(path):
         print('文件路径错误')
         exit()
